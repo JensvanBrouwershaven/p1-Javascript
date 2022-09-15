039db625d72e616d755b98bd309bc8844132b55f
JensvanBrouwershaven <9018803@student.zadkine.nl>
2022-09-15 14:58:12 +0200

    sdfdsfds

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
-//string
-document.write const myname = "jens";
-//number
-document.write let myage = "15";
-//boolean
-document.write const diploma = true;
+
+
+let num1 = 5;
+let num2 = 10;
+let answer = num1 + num2;
+console.log(answer)
+
+let num3 = 78;
+let num4 = 243;
+let answer2 = num4 - num3;
+console.log(answer2)
+
+let num5 = 78;
+let num6 = 243;
+let answer3 = num5 / num6;
+console.log(answer3)
+
+let num7 = 78;
+let num8 = 243;
+let answer4 = num7 * num8;
+console.log(answer4)
