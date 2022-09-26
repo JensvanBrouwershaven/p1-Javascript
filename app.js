4d57973e44ddc3ae0665d61271a98320dd61a557
JensvanBrouwershaven <9018803@student.zadkine.nl>
2022-09-26 18:18:34 +0200

    ouderavond

--- app.js
+++ app.js
@@ -1,21 +1,20 @@
+function color_change_red() {
+    let title = "title";
+    document.getElementById("title").style.fontWeight = "normal";
+    document.getElementById("title").style.fontSize = "3rem";
+    document.getElementById("title").style.color = "red";
+  }
 
-
-let num1 = 5;
-let num2 = 10;
-let answer = num1 + num2;
-console.log(answer)
-
-let num3 = 78;
-let num4 = 243;
-let answer2 = num4 - num3;
-console.log(answer2)
-
-let num5 = 78;
-let num6 = 243;
-let answer3 = num5 / num6;
-console.log(answer3)
-
-let num7 = 78;
-let num8 = 243;
-let answer4 = num7 * num8;
-console.log(answer4)
+  function color_change_black() {
+    let title = "title";
+    document.getElementById("title").style.fontWeight = "normal";
+    document.getElementById("title").style.fontSize = "3rem";
+    document.getElementById("title").style.color = "black";
+  }
+  function background_color() {
+    let title = "prf";
+    document.getElementById("prf").style.backgroundColor = "red";
+  }
+  function textv() {
+    document.getElementById("prf").innerText = "dit is andere kaas";
+  }
