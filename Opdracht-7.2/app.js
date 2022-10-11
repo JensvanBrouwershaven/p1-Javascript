220e689cc7f4494ddc43202f1b0bab76152953a2
JensvanBrouwershaven <9018803@student.zadkine.nl>
2022-10-11 11:29:58 +0200

    opdracht 7.1/7.2

---
+++ Opdracht-7.2/app.js
@@ -0,0 +1,12 @@
+let kost = prompt("hoeveel kost het?");
+let budget = 100;
+let product = kost;
+if(product > budget){
+    console.log("Helaas, te weinig geldt")
+}
+else if(product = budget){
+    console.log("precies genoeg")
+}
+else{
+    console.log("U heeft genoeg geldt")
+}
